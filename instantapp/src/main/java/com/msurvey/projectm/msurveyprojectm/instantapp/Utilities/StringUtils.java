8a814be8e7b66fd4da6b9fb82a18f79abf8f2aec
Brianmsurvey <brian.ngugi@msurvey.co>
2018-07-09 14:27:06 +0300

    StringUtils added

---
+++ instantapp/src/main/java/com/msurvey/projectm/msurveyprojectm/instantapp/Utilities/StringUtils.java
@@ -0,0 +1,36 @@
+package com.msurvey.projectm.msurveyprojectm.instantapp.Utilities;
+import java.util.regex.*;
+
+public class StringUtils {
+
+    //Mon Oct 03 09:47:27 UTC 2016
+
+    private static String dateRegex = "([A-Za-z]{3})\\s([A-Za-z]{3})\\s([0-9]{2})\\s([0-9]{2}):([0-9]{2}):([0-9]{2})\\s([A-Z]{2,3})\\s([0-9]{4})";
+
+    private static String month = "[A-Za-z]{3}";
+
+    private static String year = "[0-9]{4}";
+
+    public static String monthRegexChecker(String stringToCheck){
+
+        String monthRegex = "[A-Za-z]{3}";
+
+        Pattern checkRegex = Pattern.compile(monthRegex);
+
+        Matcher regexMatcher = checkRegex.matcher(stringToCheck);
+
+        return regexMatcher.group(2);
+
+    }
+
+    public static String yearRegexChecker(String stringToCheck){
+
+        String yearRegex = "[0-9]{4}";
+
+        Pattern checkRegex = Pattern.compile(yearRegex);
+
+        Matcher regexMatcher = checkRegex.matcher(stringToCheck);
+
+        return regexMatcher.group();
+    }
+}
